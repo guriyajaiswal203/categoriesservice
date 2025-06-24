7793eea935c21586ee4e18e1f65fe94ff49933e6
guriy <guriy@DESKTOP-HBO69M8>
2025-06-24 09:52:57 +0530

    service interface created

--- src/main/java/com/barclays/categories/service/ICategoriesService.java
+++ src/main/java/com/barclays/categories/service/ICategoriesService.java
@@ -1,7 +1,10 @@
 package com.barclays.categories.service;
 
+import com.barclays.categories.model.CategoriesRequest;
+import com.barclays.categories.model.CategoriesResponse;
+
 public interface ICategoriesService {
 	
-	
+	public CategoriesResponse getCategories(CategoriesRequest categoriesReq);
 
 }
